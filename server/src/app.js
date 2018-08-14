9150c0d5604ff6ed9aeaa34e6165750c1d8982ab
Jeffrey Chea <jeffreychea234@gmail.com>
2018-08-14 13:15:19 -0500

    will add functionality for nGrams sentence generator

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,13 @@ const request = require('request-promise')
 const qs = require('query-string')
 const mc = require('markovchain')
 const une = require('unescape-js')
+const Filter = require('bad-words')
+const { pride, wordGen, sentenceGen, triGramGen } = require('./generator')
+
+const clean = new Filter();
 const app = express()
+
+clean.addWords('rape', 'islam', 'white', 'nationalist', 'gay', 'Rape', 'Gay')
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -16,36 +22,38 @@ app.get('/posts', (req, res) => {
     res.send([{
       title: "hello World!",
       description: "Hi there! how are you!?"
-    }]
-  )
+    }])
 })
 
 app.post('/posts', (req, res) => {
-  let query = {q: req.body.query.split(' ').join('+')}
-
-  let options = {
-    url: 'https://api.pushshift.io/reddit/comment/search',
-    qs: query,
-    method: 'GET'
-  }
-  request(options)
+  let query = req.body.query.split(' ').join('+');
+  sentenceGen(8)
+  request.get(`https://api.pushshift.io/reddit/comment/search/?q=${query}&html_decode`)
   .then((data)=>JSON.parse(data))
   .then((data)=>data.data
-    .map((comment)=>comment.body)
-    .filter((string)=>!string.includes('#'))
-    .filter((string)=>!string.includes('|'))
-    // .map((string)=>une(string))
-    .map((string)=>string.replace([/^\w\s|'|,|.|!|/gi], ''))
-    .map((string)=>string.replace([/-|\\|\[|\]|\{|\}/gi], ''))
-    .map((string)=>string.replace([/&gt;|&gt|\n/gi], ''))
-    .map((string)=>string.replace(/(?:https?|ftp):\/\/[\n\S]+/g, ''))
-    .map((string)=>string.split('\n').join(' '))
-    .map((string)=>string.split('  ').join(' '))
-    .map((string)=>string.split('  ').join(' '))
-    .map((string)=>string.split('&gt;').join(' '))
-    .map((string)=>string.split('\'').join('')))
-  .then((text) => {console.log(text)})
-  res.send('hello')
+    .map((comment)=>comment.body.split('. '))
+    .reduce((x, y)=>x.concat(y))
+    .filter((string)=> string.includes('#') ? false : true)
+    .filter((string)=> string.includes('|') ? false : true)
+    .filter((string)=> string.includes('[') ? false : true)
+    .filter((string)=> string.includes('^') ? false : true)
+    .filter((string)=> string.includes('{') ? false : true)
+    .filter((string)=> string.includes('/') ? false : true)
+    .filter((string)=> string.includes('*') ? false : true)
+    .filter((string)=> clean.isProfaneLike(string) ? false : true)
+    .map((string)=> string.trim())
+    .map((string)=>string.replace(/^\w\s!'",.?/gi, '')
+    // .map((string)=>string.replace(/[&lt;&gt;&quot;&amp;&copy;&reg;]/gi, '')
+      .replace([/-|\\|\[|\]|\{|\}|\\\\/gi], '')
+      .replace([/&gt;|&gt|\n/gi], '')
+      .replace(/(?:https?|ftp):\/\/[\n\S]+/g, ''))
+    .map((string)=>string.split('\n').join(' ')
+      .split('  ').join(' ')
+      .split('  ').join(' ')
+      .split('&gt;').join(' ')
+      .split('\'').join('')))
+  // .then((text) => text.filter((string)=> analyzer.getSentiment(string.split(' ')) >= 0.1 ? true : false))
+  .then((text) => {res.send(text[Math.floor(Math.random() * text.length) + 1])})
 })
 
 app.listen(process.env.PORT || 8081)
---
+++ server/src/generator.js
@@ -0,0 +1,96 @@
+const natural = require('natural')
+const fs = require('fs')
+const Analyzer = natural.SentimentAnalyzer
+const stemmer = natural.PorterStemmer
+const analyzer = new Analyzer('English', stemmer, 'afinn')
+const Pride = fs.readFileSync('./pride.txt', 'utf8')
+const NGrams = natural.NGrams
+const tri = NGrams.trigrams(Pride)
+const bi = NGrams.bigrams(Pride)
+const tokenizer = new natural.WordPunctTokenizer()
+
+const tokens = tokenizer.tokenize(Pride)
+
+const wordGen = () =>
+  tokens[Math.floor(Math.random() * (tokens.length - 1)) + 1]
+
+const frequencyTable = (nGrams) =>
+  nGrams.reduce((acc, nGram)=>{
+    if (acc[nGram[0]]) {
+      acc[nGram[0]].push(nGram)
+    } else {
+      acc[nGram[0]] = [nGram]
+    }
+    return acc
+  }, {})
+
+const triFreq = frequencyTable(tri)
+const biFreq = frequencyTable(bi)
+
+const arrayEquals = (x, y) => {
+  if (x === y) return true;
+  if (x == null || y == null) return false;
+  if (x.length != y.length) return false;
+
+  for (var i = 0; i < x.length; ++i) {
+    if (x[i] !== y[i]) return false;
+  }
+  return true;
+}
+
+const sentenceGen = (length) => {
+  let result = [],
+      findStarter = () => {
+        let temp = wordGen();
+        return biFreq[temp].length > 0 ? temp : findStarter()
+      }
+      starter = findStarter();
+
+  result.push(starter)
+  while (length > 0) {
+    let wordPairs = biFreq[starter],
+        pickedPair = wordPairs[Math.floor(Math.random() * wordPairs.length)];
+        probability = function(array, pair) {
+          return array.reduce((x, y) =>
+             arrayEquals(y, pair) ? x + 1 : x, 0) / wordPairs.length
+          }(wordPairs, pickedPair);
+    if (Math.random() < probability) {
+      starter = pickedPair[1];
+      result.push(starter)
+      length--
+    }
+  }
+  return result.join(' ')
+}
+
+const triGramGen = (length) => {
+  let result = [],
+      findStarter = () => {
+        let temp = wordGen();
+        return triFreq[temp] ? temp : findStarter()
+      },
+      starter = findStarter(),
+      counter = 0;
+  result.push(starter)
+  while (length > 0) {
+    counter += 1
+    let wordTriplet = triFreq[starter],
+        pickedTriplet = wordTriplet[Math.floor(Math.random() * wordTriplet.length)];
+        probability = function(array, triplet) {
+          let totalTriplets = array.length || 0
+          return array.reduce((x, y) =>
+             arrayEquals(y, triplet) ? x + 1 : x, 0) / totalTriplets
+          }(wordTriplet, pickedTriplet);
+    if (Math.random() < probability) {
+      starter = pickedTriplet[2];
+      result.push(pickedTriplet[1], pickedTriplet[2])
+      length--
+    }
+  }
+  return result.join(' ')
+}
+
+module.exports.pride = triFreq
+module.exports.wordGen = wordGen
+module.exports.sentenceGen = sentenceGen
+module.exports.triGramGen = triGramGen
